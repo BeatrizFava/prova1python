0ac7adc85102067309745ec2f3e45f1525956c5e
BeatrizFava <00001134787753sp@al.educacao.sp.gov.br>
2026-03-31 13:09:03 -0300

    Add files via upload

---
+++ simulador.py
@@ -0,0 +1,8 @@
+simulador = "sim"
+print("Você ainda está no jogo! 🎮")
+while simulador !="nao":
+ simulador = input("deseja continuar jogando? (sim/não): ")
+ if simulador =="nao":
+    print("Game over")
+ if simulador =="sim":
+    print("Você ainda está no jogo! 🎮")
